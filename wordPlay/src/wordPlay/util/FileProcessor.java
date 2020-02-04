c705f39a56c442e5b1ac467cabd4f3731b46c034
sagar <sagar@192.168.0.17>
2020-02-03 23:53:29 -0500

    partial file validation code updated

--- wordPlay/src/wordPlay/util/FileProcessor.java
+++ wordPlay/src/wordPlay/util/FileProcessor.java
@@ -1,5 +1,53 @@
 package wordPlay.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import wordPlay.sentence.SentenceHandler;
+
 public class FileProcessor {
+
+	public boolean isValid(String fileName) {
+		File file = new File(fileName);
+		if(!file.exists()) {
+			System.out.println("File Exists");
+		}
+		return file.exists();
+	}
+
+	public boolean createIfNotExists(String fileName) {
+		File file = new File(fileName);
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (Exception ie) {
+				System.out.println("Error cocurred while creating file ["+fileName+"]:\n" + ie.getMessage());
+				ie.printStackTrace();
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public BufferedReader readFile(String filename) {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filename));
+			String lineFromFile=br.readLine();
+			SentenceHandler sc = new SentenceHandler();
+			sc.processLine(lineFromFile);
+			return br;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("Error while reading contents of file ["+filename+"]:\n" + e.getMessage());
+			return null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
 	
 }
