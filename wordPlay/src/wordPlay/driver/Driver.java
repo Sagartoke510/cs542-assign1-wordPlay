8b26a77d4450e300b3d3b8f4f72c60c5f0842d15
sagar <sagar@192.168.0.17>
2020-02-06 13:05:34 -0500

    Call method added

--- wordPlay/src/wordPlay/driver/Driver.java
+++ wordPlay/src/wordPlay/driver/Driver.java
@@ -1,6 +1,10 @@
 package wordPlay.driver;
 
+import java.io.IOException;
+
+import wordPlay.sentence.SentenceHandler;
 import wordPlay.util.FileProcessor;
+import wordPlay.util.Results;
 
 /**
  * @author Sagar Toke
@@ -8,7 +12,7 @@ import wordPlay.util.FileProcessor;
  */
 public class Driver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		/*
 		 * As the build.xml specifies the arguments as argX, in case the argument value
@@ -20,21 +24,36 @@ public class Driver {
 			System.err.println("Error: Incorrect number of arguments. Program accepts 3 arguments.");
 			System.exit(0);
 		}
-
-		FileProcessor fp = new FileProcessor();
-		if (!fp.isValid(args[0])) {
-			System.out.println("Input file [" + args[0] + "] does not exist, please provide a valid input filename");
-			System.exit(0);
-		}
+		
+		FileProcessor.Validators.fileValidator(args[0]).run();
 
 		String filename = null;
-		if (!fp.createIfNotExists(filename = args[1]) ||
-			!fp.createIfNotExists(filename = args[2])) {
+		if (!FileProcessor.createIfNotExists(filename = args[1]) ||
+			!FileProcessor.createIfNotExists(filename = args[2])) {
 			System.out.println("Failed to create output file named [" + filename + "]");
 			System.exit(0);
 		}
+		
 
 		System.out.println("Hello World! Lets get started with the assignment");
+		
+		try (
+				FileProcessor fp = new FileProcessor(args[0]);
+				Results res = new Results(args[1], args[2]);
+			) {
+			SentenceHandler sh = new SentenceHandler();
+			String line;
+			while (fp.next()) {
+				line = sh.processLine(fp.getLine());
+				res.writeToFile(args[1], line);
+				res.writeToStdOut(line);
+			}
+			res.writeToFile(args[2], sh.getStats());
+		} catch (IOException iox) {
+			iox.printStackTrace();
+		}
+		
+
 
 	}
 
