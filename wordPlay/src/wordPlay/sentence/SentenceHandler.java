aae38d4fb9cb6630e2fbe31a7be80af1c0681665
sagar <sagar@192.168.0.17>
2020-02-06 12:40:43 -0500

    word count logic and partial statement logic handled

--- wordPlay/src/wordPlay/sentence/SentenceHandler.java
+++ wordPlay/src/wordPlay/sentence/SentenceHandler.java
@@ -1,34 +1,51 @@
 package wordPlay.sentence;
 
-import wordPlay.stats.StatsCollector;
+
+import wordPlay.stats.StatsProcessor;
 
 public class SentenceHandler {
 	
-	private final StatsCollector stats;
-	
+	private final StatsProcessor stats;
 	private String partialSentence = null;
+	private StringBuilder sb;
+	SentenceUtils sentUtils;
 
 	public SentenceHandler() {
-		stats = new StatsCollector();
+		stats = new StatsProcessor();
+		sb = new StringBuilder();
+		sentUtils = new SentenceUtils();
 	}
 
-	public void processLine(String lineFromFile) {
-		int i = -1;
+	public String processLine(String lineFromFile) {
+//		int i = -1;
 		String[] words;
+		String lineWithoutDots = lineFromFile.replaceAll("\\.", "");
+		int sentenceCount = lineFromFile.length() - lineWithoutDots.length();
 		for (String sentence : lineFromFile.split("\\.")) {
 			if (partialSentence != null) {
 //				partialSentence += sentence;
 				sentence = partialSentence + sentence;
 				partialSentence = null;
 			}
-			if (sentence.endsWith("\n")) {
-				if (partialSentence.length() > 1) partialSentence = sentence;
+			if (sentenceCount-- <= 0) {
+				if (partialSentence == null) partialSentence = sentence;
+				else partialSentence += sentence + "\n";
 				break;
 			}
 			
-			words = sentence.split("\\s+");
-			stats.calculateStats(sentence);
-			
+			stats.calculateStats(sentence + ".");
+			for (String word : words = sentence.split("^\\s\\r\\n")) {
+				sb.append( sentUtils.reverse(word) ).append(" ");
+			}
+			if (sb.length() > 0) sb.deleteCharAt(sb.length() - 1).append(".");
 		}
+		String returnVal = sb.toString();
+		sb.setLength(0);
+		return returnVal;
 	}
+	
+	public String getStats() {
+		return stats.getAll();
+	}
+	
 }
